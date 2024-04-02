af92011f5a7045db4f4af4cfe4fa32c41bf02dd4
Pramod J M <pramodjm4@gmail.com>
2024-04-02 18:34:31 +0530

    testing swagger, by changing cdn

--- config/swaggerFiles.js
+++ config/swaggerFiles.js
@@ -38,7 +38,7 @@ const swaggerOption = {
 }
 
 const CSS_URL =
-    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+    "https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui.css";
 
 const swaggerSpec = swaggerJSDoc(swaggerOption)
 
